5a58da126b42d076f6d65d020192c1b8c44c5125
ong-ar <reclusis@gmail.com>
2019-03-13 20:22:22 +0900

    test middleware

--- index.ts
+++ index.ts
@@ -2,8 +2,24 @@ import * as express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log(req);
+interface Request extends express.Request {
+  requestTime?: String | Number;
+}
+
+const requestTime = (
+  req: Request,
+  _: express.Response,
+  next: express.NextFunction
+): void => {
+  console.log("logged");
+  req.requestTime = Date.now();
+  next();
+};
+
+app.use(requestTime);
+
+app.get("/", (req: Request, res: express.Response) => {
+  console.log(req.requestTime);
   res.send("hello world!");
 });
 
