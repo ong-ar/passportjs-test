cf50be9b0a6c2726eaa8f8f0bcdca6f82bb6a4d2
ong-ar <reclusis@gmail.com>
2019-03-15 13:30:55 +0900

    facebook login

--- README.md
+++ README.md
@@ -5,4 +5,8 @@
 ```
 $ yarn add express
 $ yarn add @types/express --dev
+
+$ yarn add passport passport-facebook
 ```
+
+- ssl 필수
--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import * as passport from "passport";
+import { Strategy as FacebookStrategy } from "passport-facebook";
 
 const app: express.Application = express();
 
@@ -16,6 +18,27 @@ const requestTime = (
   next();
 };
 
+passport.use(new FacebookStrategy({
+  clientID: '',
+  clientSecret: '',
+  callbackURL: "http://localhost/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  console.log(accessToken, refreshToken, profile)
+  done(null, accessToken);
+}
+));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
 app.use(requestTime);
 
 app.get("/", (req: Request, res: express.Response) => {
@@ -23,4 +46,4 @@ app.get("/", (req: Request, res: express.Response) => {
   res.send("hello world!");
 });
 
-app.listen(3000, () => console.log("example app listening on port 3000!"));
+app.listen(8000, () => console.log("example app listening on port 3000!"));
